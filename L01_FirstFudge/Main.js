da802015dca536c27d5c78a6dc4963f4020de8a8
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-10-13 18:21:20 +0200

    changed "ƒ" to "f"

--- L01_FirstFudge/Main.js
+++ L01_FirstFudge/Main.js
@@ -1,25 +1,25 @@
 "use strict";
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
-    var ƒ = FudgeCore;
+    var f = FudgeCore;
     console.log("Hallo");
     window.addEventListener("load", hndLoad);
     function hndLoad(_event) {
         const canvas = document.querySelector("canvas");
-        ƒ.Debug.log(canvas);
-        let node = new ƒ.Node("Quad");
-        let mesh = new ƒ.MeshQuad();
-        let cmpMesh = new ƒ.ComponentMesh(mesh);
+        f.Debug.log(canvas);
+        let node = new f.Node("Quad");
+        let mesh = new f.MeshQuad();
+        let cmpMesh = new f.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
-        let mtrSolidWhite = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
-        let cmpMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        let mtrSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let cmpMaterial = new f.ComponentMaterial(mtrSolidWhite);
         node.addComponent(cmpMaterial);
-        let cmpCamera = new ƒ.ComponentCamera();
+        let cmpCamera = new f.ComponentCamera();
         cmpCamera.pivot.translateZ(2);
         cmpCamera.pivot.rotateY(180);
-        L01_FirstFudge.viewport = new ƒ.Viewport();
+        L01_FirstFudge.viewport = new f.Viewport();
         L01_FirstFudge.viewport.initialize("Viewport", node, cmpCamera, canvas);
-        ƒ.Debug.log(L01_FirstFudge.viewport);
+        f.Debug.log(L01_FirstFudge.viewport);
         L01_FirstFudge.viewport.draw();
     }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
--- L01_FirstFudge/Main.ts
+++ L01_FirstFudge/Main.ts
@@ -1,32 +1,32 @@
 namespace L01_FirstFudge {
-    import ƒ = FudgeCore;
+    import f = FudgeCore;
 
     console.log("Hallo");
 
     window.addEventListener("load", hndLoad);
-    export let viewport: ƒ.Viewport;
+    export let viewport: f.Viewport;
 
     function hndLoad(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        ƒ.Debug.log(canvas);
+        f.Debug.log(canvas);
 
-        let node: ƒ.Node = new ƒ.Node("Quad");
+        let node: f.Node = new f.Node("Quad");
 
-        let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
-        let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+        let mesh: f.MeshQuad = new f.MeshQuad();
+        let cmpMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
         
-        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
-        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        let mtrSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(mtrSolidWhite);
         node.addComponent(cmpMaterial);
 
-        let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+        let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
         cmpCamera.pivot.translateZ(2);
         cmpCamera.pivot.rotateY(180);
 
-        viewport = new ƒ.Viewport();
+        viewport = new f.Viewport();
         viewport.initialize("Viewport", node, cmpCamera, canvas);
-        ƒ.Debug.log(viewport);
+        f.Debug.log(viewport);
 
         viewport.draw();
     }
