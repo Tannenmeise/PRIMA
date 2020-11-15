2ed1eb5cb03aea632416ef1b0b4850abcdd96a90
Tamara Hezel <tamara.hezel@hs-furtwangen.de>
2020-11-15 23:17:15 +0100

    L09 collision

--- L09_Doom_Control/Main.js
+++ L09_Doom_Control/Main.js
@@ -6,6 +6,7 @@ var L09_Doom_Control;
     window.addEventListener("load", hndLoad);
     let root = new f.Node("Root");
     let avatar = new f.Node("Avatar");
+    let wall;
     let ctrSpeed = new f.Control("AvatarSpeed", 1, 0 /* PROPORTIONAL */);
     ctrSpeed.setDelay(100);
     let ctrRotation = new f.Control("AvatarRotation", 3, 0 /* PROPORTIONAL */);
@@ -21,8 +22,10 @@ var L09_Doom_Control;
         root.appendChild(floor);
         let txtWall = new f.TextureImage("../DoomAssets/CEMPOIS.png");
         let mtrWall = new f.Material("Wall", f.ShaderTexture, new f.CoatTextured(null, txtWall));
-        let wall = new L09_Doom_Control.Wall(f.Vector2.ONE(3), f.Vector3.Y(1.5), f.Vector3.ZERO(), mtrWall);
+        // #region (Walls) 
+        wall = new L09_Doom_Control.Wall(f.Vector2.ONE(3), f.Vector3.Y(1.5), f.Vector3.ZERO(), mtrWall);
         root.appendChild(wall);
+        // #endregion (Walls)
         let cmpCamera = new f.ComponentCamera();
         cmpCamera.pivot.translate(f.Vector3.Y(1.7));
         cmpCamera.backgroundColor = f.Color.CSS("darkblue");
@@ -37,13 +40,40 @@ var L09_Doom_Control;
         f.Loop.start(f.LOOP_MODE.TIME_GAME, 60);
     }
     function hndLoop(_event) {
-        ctrSpeed.setInput(f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.S, f.KEYBOARD_CODE.ARROW_DOWN])
-            + f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.W, f.KEYBOARD_CODE.ARROW_UP]));
-        ctrRotation.setInput(f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.A, f.KEYBOARD_CODE.ARROW_LEFT])
-            + f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.D, f.KEYBOARD_CODE.ARROW_RIGHT]));
+        ctrSpeed.setInput(f.Keyboard.mapToValue(-0.2, 0, [f.KEYBOARD_CODE.S, f.KEYBOARD_CODE.ARROW_DOWN])
+            + f.Keyboard.mapToValue(0.2, 0, [f.KEYBOARD_CODE.W, f.KEYBOARD_CODE.ARROW_UP]));
+        ctrRotation.setInput(f.Keyboard.mapToValue(0.7, 0, [f.KEYBOARD_CODE.A, f.KEYBOARD_CODE.ARROW_LEFT])
+            + f.Keyboard.mapToValue(-0.7, 0, [f.KEYBOARD_CODE.D, f.KEYBOARD_CODE.ARROW_RIGHT]));
         avatar.mtxLocal.translateZ(ctrSpeed.getOutput());
         avatar.mtxLocal.rotateY(ctrRotation.getOutput());
+        console.log(wall.mtxLocal.getZ());
+        // Check Collision:
+        if (checkCollision(wall)) {
+            console.log("Collision = true");
+            let tempPos = avatar.mtxLocal.translation;
+            tempPos.x += wall.mtxLocal.getZ().x * 0.1;
+            tempPos.z += wall.mtxLocal.getZ().z * 0.1;
+            avatar.mtxLocal.translation = tempPos;
+        }
+        /*
+      for (let a_wall of wall.getChildren() as faid.Node[]) {
+
+        if (checkCollision(a_wall)) {
+            let tempPos: f.Vector3 = avatar.mtxLocal.translation;
+            tempPos.x += wall.mtxLocal.getZ().x * 0.001;
+            tempPos.z += wall.mtxLocal.getZ().z * 0.001;
+            avatar.mtxLocal.translation = tempPos;
+        }
+      }
+      */
         L09_Doom_Control.viewport.draw();
+    }
+    function checkCollision(_target) {
+        let distance = (avatar.mtxWorld.translation.x - _target.mtxWorld.translation.x) * _target.mtxLocal.getZ().x + (avatar.mtxWorld.translation.y - _target.mtxWorld.translation.y) * _target.mtxLocal.getZ().y + (avatar.mtxWorld.translation.z - _target.mtxWorld.translation.z) * _target.mtxLocal.getZ().z;
+        if (distance < 1.6) {
+            return true;
+        }
+        return false;
     }
 })(L09_Doom_Control || (L09_Doom_Control = {}));
 //# sourceMappingURL=Main.js.map
--- L09_Doom_Control/Main.ts
+++ L09_Doom_Control/Main.ts
@@ -7,6 +7,8 @@ namespace L09_Doom_Control {
     export let viewport: f.Viewport;
     let root: f.Node = new f.Node("Root");
     let avatar: f.Node = new f.Node("Avatar");
+
+    let wall: Wall;
   
     let ctrSpeed: f.Control = new f.Control("AvatarSpeed", 1, f.CONTROL_TYPE.PROPORTIONAL);
     ctrSpeed.setDelay(100);
@@ -29,8 +31,10 @@ namespace L09_Doom_Control {
       let txtWall: f.TextureImage = new f.TextureImage("../DoomAssets/CEMPOIS.png");
       let mtrWall: f.Material = new f.Material("Wall", f.ShaderTexture, new f.CoatTextured(null, txtWall));
   
-      let wall: Wall = new Wall(f.Vector2.ONE(3), f.Vector3.Y(1.5), f.Vector3.ZERO(), mtrWall);
+      // #region (Walls) 
+      wall = new Wall(f.Vector2.ONE(3), f.Vector3.Y(1.5), f.Vector3.ZERO(), mtrWall);
       root.appendChild(wall);
+      // #endregion (Walls)
   
       let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
       cmpCamera.pivot.translate(f.Vector3.Y(1.7));
@@ -49,18 +53,55 @@ namespace L09_Doom_Control {
     }
   
     function hndLoop(_event: Event): void {
+      
       ctrSpeed.setInput(
-        f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.S, f.KEYBOARD_CODE.ARROW_DOWN])
-        + f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.W, f.KEYBOARD_CODE.ARROW_UP])
+        f.Keyboard.mapToValue(-0.2, 0, [f.KEYBOARD_CODE.S, f.KEYBOARD_CODE.ARROW_DOWN])
+        + f.Keyboard.mapToValue(0.2, 0, [f.KEYBOARD_CODE.W, f.KEYBOARD_CODE.ARROW_UP])
       );
       ctrRotation.setInput(
-        f.Keyboard.mapToValue(1, 0, [f.KEYBOARD_CODE.A, f.KEYBOARD_CODE.ARROW_LEFT])
-        + f.Keyboard.mapToValue(-1, 0, [f.KEYBOARD_CODE.D, f.KEYBOARD_CODE.ARROW_RIGHT])
+        f.Keyboard.mapToValue(0.7, 0, [f.KEYBOARD_CODE.A, f.KEYBOARD_CODE.ARROW_LEFT])
+        + f.Keyboard.mapToValue(-0.7, 0, [f.KEYBOARD_CODE.D, f.KEYBOARD_CODE.ARROW_RIGHT])
       );
   
       avatar.mtxLocal.translateZ(ctrSpeed.getOutput());
       avatar.mtxLocal.rotateY(ctrRotation.getOutput());
+
+     
+      console.log(wall.mtxLocal.getZ());
+      // Check Collision:
+
+      if (checkCollision(wall)) {
+        console.log("Collision = true");
+        let tempPos: f.Vector3 = avatar.mtxLocal.translation;
+        tempPos.x += wall.mtxLocal.getZ().x * 0.1;
+        tempPos.z += wall.mtxLocal.getZ().z * 0.1;
+        
+        avatar.mtxLocal.translation = tempPos;
+      }
+        /*
+      for (let a_wall of wall.getChildren() as faid.Node[]) {
+
+        if (checkCollision(a_wall)) {
+            let tempPos: f.Vector3 = avatar.mtxLocal.translation;
+            tempPos.x += wall.mtxLocal.getZ().x * 0.001;
+            tempPos.z += wall.mtxLocal.getZ().z * 0.001;
+            avatar.mtxLocal.translation = tempPos;
+        }
+      }
+      */
+
   
       viewport.draw();
     }
+
+    function checkCollision(_target: f.Node): boolean {
+
+      let distance: number = (avatar.mtxWorld.translation.x - _target.mtxWorld.translation.x) * _target.mtxLocal.getZ().x + (avatar.mtxWorld.translation.y - _target.mtxWorld.translation.y) * _target.mtxLocal.getZ().y + (avatar.mtxWorld.translation.z - _target.mtxWorld.translation.z) * _target.mtxLocal.getZ().z;
+      if (distance < 1.6) {
+        return true;
+      }
+  
+      return false;
+    }
+
   }
